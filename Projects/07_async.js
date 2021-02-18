c205e01a3298b0e970ac23ca63fc041befd8452b
integralpir <roma_5712@mail.ru>
2021-02-18 22:15:58 +0500

    '18.02.21'

---
+++ Projects/07_async.js
@@ -0,0 +1,57 @@
+// Event Loop
+
+// const timeout = setTimeout(() => {
+//     clearTimeout(timeout) // очищает timeout, функция вызовется 1 раз
+//     console.log('After timeout')
+// }, 2500)
+//
+// clearTimeout(timeout) // функция не вызовется вообще
+
+// const interval = setInterval(() => {
+//     console.log('After interval')
+// }, 2500)
+
+// clearInterval(interval)
+// const delay = (callback, wait = 1000) => {
+//     setTimeout(callback, wait)
+// }
+//
+// delay(() => {
+//     console.log('After 2 seconds')
+// }, 2000)
+
+const delay = (wait = 1000) => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject('Что-то пошло не так.')
+        }, wait)
+    })
+    return promise
+}
+//
+// delay(2500)
+//     .then(() => {
+//         console.log('after 2 seconds')
+//     })
+//     .catch(err => console.error('Error:',err))
+//     .finally(() => console.log('Finally'))
+
+const getData = () => new Promise(resolve => resolve([
+    1, 1, 2, 3, 5, 8, 13
+]))
+
+getData().then(data => console.log(data))
+
+async function asyncExample() {
+    try {
+        await delay(3000)
+        const data = await getData()
+        console.log(data)
+    } catch (e) {
+        console.log(e)
+    } finally {
+        console.log('Finally')
+    }
+}
+
+asyncExample()
